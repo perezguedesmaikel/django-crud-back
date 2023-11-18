8df561b1b265aa644a01a91aa3daa1049668d7f2
perezguedesmaikel <perezguedesmaikel@gmail.com>
2023-11-18 01:04:35 -0500

    ready two

--- myCrub/views.py
+++ myCrub/views.py
@@ -1,8 +1,9 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 
 from .serializers import ProductoSerializer
 from .models import Producto
 
-class ProductoCreateView(CreateAPIView):
+
+class ProductoCreateView(ListCreateAPIView):
     serializer_class = ProductoSerializer
     queryset = Producto.objects.all()
